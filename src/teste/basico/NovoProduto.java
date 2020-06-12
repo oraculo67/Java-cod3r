bd115b83529e5d6cf0241ec14abe84347afdd949
oraculo67 <oraculo_lxvii@outlook.com>
2020-06-12 08:50:27 -0300

    Projeto Criado

---
+++ src/teste/basico/NovoProduto.java
@@ -0,0 +1,15 @@
+package teste.basico;
+
+import infra.DAO;
+import modelo.basico.Produto;
+
+public class NovoProduto {
+
+	public static void main(String[] args) { 
+		
+		Produto produto = new Produto("Caneta", 7.45);
+		
+		DAO<Produto> dao = new DAO<Produto>(Produto.class);
+		dao.abrirT().incluir(produto).fecharT().fechar();
+	}
+}
